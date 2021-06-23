b86f2935f5278acd213779e543f8efd4c6ecb4aa
SeraphDev6 <82985907+SeraphDev6@users.noreply.github.com>
2021-06-23 12:01:29 -0400

    Update Task_manager.rb

--- Task_manager.rb
+++ Task_manager.rb
@@ -1,20 +1,22 @@
 module Task_manager
   #Save and Load the CSV
   def Load(path="to_do_list.csv")
-  $data=CSV.read(path)
+    CSV.foreach(path,headers:true) do |row|
+      ntask=Task.new(row["Name"],row["Description"],row["Due Date"],row["Priority"])
+    end
   end
+#----------------------------------------------------------
   def Save(path="to_do_list.csv")
     CSV.open(path,"wb") do |csv|
       #add headers
-      if $data[0] !=["Name", "Description", "Due Date", "Priority"]
       csv<<["Name", "Description", "Due Date", "Priority"]
-      end
       #add data
-     for line in $data
-       csv << line
+     for task in $data
+       csv << [task.name,task.description,task.due_date,task.priority]
       end
     end
   end
+#----------------------------------------------------------
   #Help Menu
   def Help()
     puts "Type 'Add' to add a task"
@@ -34,14 +36,89 @@ module Task_manager
     puts "What is the Task's Priority, with 1 being highest, and 10 being lowest?"
     priority=STDIN.gets().chomp
     ntask=Task.new(name,desc,due,priority)
+    Save()
     puts "Task #{name} successfully added."
   end
+#----------------------------------------------------------
   def Finish()
+    puts "Which Task did you finish?"
+    input=STDIN.gets().chomp.downcase
+    if $data.any? {|task| task.name.downcase == input}
+      $data.delete_if {|task| task.name.downcase == input}
+      Save()
+      puts "Good job completing #{input.capitalize}!" 
+    elsif $data.any? {|task| task.name.downcase.include? input}
+      for task_obj in $data.select {|task|  task.name.downcase.include? input}
+        puts "Did you mean #{task_obj.name}?"
+        puts "If so type 'y'"
+        input=STDIN.gets().chomp.downcase
+        if input == "y"
+          $data.delete(task_obj)
+          Save()
+          puts "Good job completing #{task_obj.name}!"
+          return
+        else 
+          puts "Okay.."
+        end
+      end
+      puts "Sorry, I couldn't find that Task."
+      puts "Returning to Main Menu"
+    else
+      puts "Hmm.. I couldn't find that task. Returning to main menu."
+    end
   end
+#----------------------------------------------------------
   def List()
+    for task in $data
+      puts task.name+"  Due By: " +task.due_date+"  Priority: "+task.priority
+    end
   end
+#----------------------------------------------------------
   def Sort()
+    puts "What would you like to sort your tasks by?"
+    puts "Type 'Name', 'Description', 'Due Date', or 'Priority'"
+    input=STDIN.gets.chomp.downcase
+    case input
+    when "name"
+      $data.sort_by!{|task| task.name.downcase}
+    when "description","desc"
+      $data.sort_by!{|task| task.description.downcase}
+    when "due date","due","date"
+      $data.sort_by!{|task| task.due_date.downcase}
+    when "priority"
+      $data.sort_by!{|task| task.priority.downcase}
+    else 
+      puts "Hmm... I'm not sure what to sort by."
+      puts "Returning to Main Menu"
+      return
+    end
+    Save()
+    puts "Tasks successfully sorted by #{input.capitalize}!"
   end
+#----------------------------------------------------------
   def Details()
+    puts "Which Task do you want details on?"
+    input=STDIN.gets().chomp.downcase
+    if $data.any? {|task| task.name.downcase == input}
+
+    elsif $data.any? {|task| task.name.downcase.include? input}
+      for task_obj in $data.select {|task|  task.name.downcase.include? input}
+        puts "Did you mean #{task_obj.name}?"
+        puts "If so type 'y'"
+        input=STDIN.gets().chomp.downcase
+        if input == "y"
+          puts "Task Name: "+task_obj.name
+          puts "Task Description: "+task_obj.description
+          puts "Task Due Date: "+task_obj.due_date
+          puts "Task Priority: "+task_obj.priority
+          return
+        else 
+          puts "Okay.."
+        end
+      end
+    end
+    puts "Sorry, I couldn't find that Task."
+    dputs "Returning to Main Menu"
   end  
 end
+
