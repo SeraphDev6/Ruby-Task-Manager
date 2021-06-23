2290b0757aad18f71a8865a65db10aed89f49bbf
SeraphDev6 <82985907+SeraphDev6@users.noreply.github.com>
2021-06-23 11:53:33 -0400

    Update Task.rb

--- Task.rb
+++ Task.rb
@@ -1,7 +1,15 @@
 #Tasks to do
 class Task
   def initialize(name, description, due, priority)
-    $data.push([name, description, due, priority])
-    Save()
+    @name=name
+    @description=description
+    @due_date=due
+    @priority=priority
+    $data.push(self)
   end
+  attr_reader :name
+  attr_reader :description
+  attr_reader :due_date
+  attr_reader :priority
+
 end
